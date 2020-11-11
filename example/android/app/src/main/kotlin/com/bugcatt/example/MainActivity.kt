3f2d69e0ca64b7de1b65816a98d529b41ccc3518
harrisonqi <wb-qyh825749@antgroup.com>
2020-11-11 16:45:25 +0800

    :sparkles: 提升flutool质量分

--- example/android/app/src/main/kotlin/com/bugcatt/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.bugcatt.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutool_core/flutool_core.dart';
+
+void main() {
+  // StrUtil
+  // 字符串工具类
+  bool hasEmpty = StrUtil.hasEmpty(['1', 'a', '']);
+  print('hasEmpty:$hasEmpty');
+}
