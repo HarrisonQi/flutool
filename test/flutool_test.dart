6c4891a0ab6d54613f27d1b72463c4c59be34285
harrisonqi <wb-qyh825749@antgroup.com>
2020-11-11 11:39:21 +0800

    :sparkles: 更新flutool test

--- test/flutool_test.dart
+++ test/flutool_test.dart
@@ -1,13 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutool/flutool.dart';
+import 'package:flutool_core/flutool_core.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('Test of StrUtil', () {
+    expect(''.isBlank, true);
   });
 }
